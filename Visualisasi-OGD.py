f1ec5cc9ccf6128d747e51cd146a30aaf0a8eb12
adityoAJA <168532434+adityoAJA@users.noreply.github.com>
2024-07-05 11:23:52 +0700

    Update Visualisasi-OGD.py

--- Visualisasi-OGD.py
+++ Visualisasi-OGD.py
@@ -50,9 +50,12 @@ with tabs[0]:
                         progress_bar.empty()
                         st.success(f"Memotong {fname} sesuai koordinat terpilih")
         
-                        with xr.open_dataset(temp_file_path, engine='netcdf4', decode_times=False) as data:
-                            data['time'] = pd.date_range(start=str(iy)+'-01-01', end=str(iy)+'-12-31', periods=len(data.time))
-                            sliced_data = data.sel(longitude=slice(longitude[0], longitude[1]), latitude=slice(latitude[0], latitude[1]))
+                        try:
+                                with xr.open_dataset(temp_file_path, engine='netcdf4', decode_times=False) as data:
+                                    data['time'] = pd.date_range(start=str(iy)+'-01-01', end=str(iy)+'-12-31', periods=len(data.time))
+                                    sliced_data = data.sel(longitude=slice(longitude[0], longitude[1]), latitude=slice(latitude[0], latitude[1]))
+                        except Exception as e:
+                        st.error(f"Kesalahan dalam memproses {fname}: {e}")
                             
                             with tempfile.NamedTemporaryFile(delete=False, suffix=".nc") as final_tmp:
                                 sliced_data.to_netcdf(final_tmp.name)  # Save sliced data to a temporary file
