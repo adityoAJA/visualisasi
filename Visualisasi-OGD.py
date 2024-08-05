033b913b22479cbc4ebe366b8a864af1bc1c4a12
adityoAJA <168532434+adityoAJA@users.noreply.github.com>
2024-08-05 10:15:22 +0700

    Update Visualisasi-OGD.py

--- Visualisasi-OGD.py
+++ Visualisasi-OGD.py
@@ -1,244 +1,153 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import xarray as xr
-import plotly.express as px
-import plotly.graph_objects as go
-import io
+import requests
+import os
+import tempfile
+from auth import authenticate
+from datetime import datetime, timedelta
 
-# Set layout
-st.set_page_config(
-        page_title="Dashboard Perubahan Iklim",
-        page_icon="🏠",
-        layout="centered",
-        initial_sidebar_state="expanded"
-    )
+# fungsi komponen download
+def download_and_process_data(dataname, varname, resolution, longitude, latitude, start_year, end_year):
 
-# judul
-st.header("Visualisasi Data Reanalysis dan Proyeksi")
-st.empty()
-# uploader untuk file netCDF
-uploaded_file = st.file_uploader("Unggah file", type=["nc"])
-# narasi mengenai data yang diupload
-with st.expander(":blue-background[**Keterangan :**]"):
-		st.caption("**Jenis data yang bisa diunggah hanya untuk parameter curah hujan, dengan kriteria yaitu :**")
-		st.caption("**Kriteria 1 (Data CHIRPS):** *Pastikan nama variabel dalam file nc meliputi precip, latitude, longitude.*")
-		st.caption("**Kriteria 2 (Data CMIP6) :** *Pastikan nama variabel dalam file nc meliputi pr, lat, lon.*")
-		st.caption('Jika anda ingin mendownload data proyeksi (CMIP6), silahkan klik tombol di bawah ini')
-		st.link_button("Unduh Data Proyeksi", "https://aims2.llnl.gov/search/cmip6/")
+    # mendefinisikan URL
+    if dataname == 'CHIRTS':
+        template = f'https://data.chc.ucsb.edu/products/CHIRTSdaily/v1.0/global_netcdf_p05/{varname}/'
+    elif dataname == 'CHIRPS':
+        if resolution == 'p05':
+            template = 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/p05/'
+        elif resolution == 'p25':
+            template = 'https://data.chc.ucsb.edu/products/CHIRPS-2.0/global_daily/netcdf/p25/'
 
-if uploaded_file is not None:
-	# membaca file yang diupload
-	file_content = uploaded_file.read()
+    # mengulangi untuk pilihan tahun > 1
+    for iy in range(start_year, end_year + 1):
+        # konstruksi nama file
+        if dataname == 'CHIRTS':
+            fname = f'{varname}.{iy}.nc'
+        elif dataname == 'CHIRPS':
+            fname = f'chirps-v2.0.{iy}.days_{resolution}.nc'
 
-	# fungsi load data
-	@st.cache_data
-	def load_data(file_content):
-		try:
-        		data = xr.open_dataset(io.BytesIO(file_content), decode_times=True, engine='h5netcdf')
-   		except ValueError as e:
-        		st.warning(f"Failed to decode times with 'decode_times=True': {e}")
-        		data = xr.open_dataset(io.BytesIO(file_content), decode_times=False, engine='h5netcdf')
-		return data.load()
+        # Download the file
+        link = template + fname
+        st.info(f"Sedang mengunduh {fname} dari server")
+        response = requests.get(link, stream=True)
 
-	# membaca data yang diupload
-	data = load_data(file_content)
+        # mengakses HTML / koneksi       
+        if response.status_code == 200:
+            total_size = int(response.headers.get('content-length', 0))
+            chunk_size = 1024
+            progress_bar = st.progress(0)
+            temp_file_path = tempfile.NamedTemporaryFile(delete=False).name
 
-	# mendefinisikan nama dari data
-	if 'precip' in data:
-		pr_var = 'precip'
-		lat_var = 'latitude'
-		lon_var = 'longitude'
-		name_var = 'Reanalysis'
-	elif 'pr' in data:
-		pr_var = 'pr'
-		lat_var = 'lat'
-		lon_var = 'lon'
-		name_var = 'Proyeksi'
-	else:
-		st.error("Tidak ada variabel yang sesuai kriteria dalam file netCDF anda.")
-		st.stop()
+            try:
+                # membuat temporary file
+                with open(temp_file_path, 'wb') as tmp_file:
+                    for data in response.iter_content(chunk_size):
+                        tmp_file.write(data)
+                        progress_bar.progress(tmp_file.tell() / total_size)
+                
+                progress_bar.empty()
+                st.success(f"Memotong {fname} sesuai koordinat terpilih")
 
-	# Extrak parameter yang diperlukan
-	lat = data[lat_var].values
-	lon = data[lon_var].values
-	pr = data[pr_var]
+                # memotong data sesuai koordinat terpilih
+                with xr.open_dataarray(temp_file_path, decode_times=False) as data:
+                    data['time'] = pd.date_range(start=f"{iy}-01-01", end=f"{iy}-12-31", periods=len(data.time))
+                    sliced_data = data.sel(longitude=slice(longitude[0], longitude[1]), latitude=slice(latitude[0], latitude[1]))
 
-	# konversi data bila proyeksi
-	if pr_var == 'pr':
-		pr *= 86400
+                    # menyimpan data pada temporary file
+                    with tempfile.NamedTemporaryFile(suffix='.nc', delete=False) as tmp_file:
+                        sliced_data.to_netcdf(tmp_file.name)
+                        final_tmp_path = tmp_file.name
 
-	# akumulasi bulanan
-	pr_monthly_sum = pr.resample(time='1MS').sum(dim='time')
+                    # menyimpan informasi pada session state
+                    if 'download_files' not in st.session_state:
+                        st.session_state['download_files'] = []
+                    st.session_state['download_files'].append((final_tmp_path, f"{varname}_{iy}_{resolution}.nc"))
 
-	# membuat daftar bulan
-	available_months = pr_monthly_sum['time'].dt.strftime('%B %Y').values
+            except Exception as e:
+                st.error(f"Kesalahan dalam memproses {fname}: {e}")
 
-	# memilih bulan yang akan ditampilkan
-	selected_month = st.selectbox('Pilih Bulan', available_months)
+            finally:
+                try:
+                    os.remove(temp_file_path)  # membersihkan temporary file
+                except Exception as e:
+                    st.error(f"Kesalahan dalam menghapus temporary file: {e}")
+        else:
+            st.error(f"Gagal mengunduh {fname} dari {link}")
 
-	# pilih data pr sesuai bulan tepilih
-	pr_selected = pr_monthly_sum.sel(time=selected_month)
+# fungsi display download
+def main():
+    st.header('Download Data Reanalysis Otomatis')
 
-	# mendefinisikan ranges and labels curah hujan
-	precip_ranges = [-np.inf, 50, 150, 300, 500, 750, np.inf]
-	precip_labels = ['<50 mm', '50-150 mm', '150-300 mm', '300-500 mm', '500-750 mm', '>750 mm']
+    # memilih parameter
+    varname = st.selectbox('Pilih Parameter', ['Precipitation', 'Tmax', 'Tmin'])
+    
+    # penjelasan singkat mengenai parameter terpilih
+    if varname == 'Precipitation':
+        dataname = 'CHIRPS'
+        with st.expander("**Keterangan :**"):
+            st.caption(f"*Dataset yang digunakan :* **{dataname}.**")
+            st.caption("**Deskripsi :** *Data Curah Hujan Harian Global.*")
+            st.caption('''
+                        **p05 :**
+                            *Resolusi Tinggi 5 x 5 km (ukuran file 1-2 GB).*
+                        ''')
+            st.caption('''
+                        **p25 :**
+                            *Resolusi Menengah 25 x 25 km (ukuran file 60-70 MB).*
+                        ''')
+        # memilih resolusi
+        resolution = st.selectbox('Pilih Resolusi', ['p05', 'p25'])
+    else:
+        dataname = 'CHIRTS'
+        with st.expander("**Keterangan :**"):
+            st.caption(f"*Dataset yang digunakan :* **{dataname}.**")
+            st.caption("**Deskripsi :** *Data Suhu Udara Maksimum (Tmax) atau Minimum (Tmin) Harian Global.*")
+            st.caption("**Resolusi :** *5 x 5 km (ukuran file 25 GB).*")
+        resolution = None  # tidak ada pilihan untuk CHIRTS dataset
 
-	# koordinat 2 dimensi
-	lat_flat = lat.repeat(len(lon))
-	lon_flat = np.tile(lon, len(lat))
-	pr_flat = pr_selected.values.flatten()
+    # komponen slider / memilih koordinat
+    longitude = st.slider('Pilih Rentang Bujur', min_value=90.0, max_value=145.0, value=(105.0, 125.0), step=0.1)
+    latitude = st.slider('Pilih Rentang Lintang', min_value=-12.0, max_value=8.0, value=(-5.0, 7.0), step=0.1)
+    # narasi terkait koordinat
+    with st.expander("**Keterangan :**"):
+        st.caption("*Default Rentang wilayah yang digunakan adalah Lintang dan Bujur di wilayah Indonesia.*")
+        st.caption("*Data yang akan tersimpan akan dipotong sesuai pilihan Lintang dan Bujur yang diinginkan.*")
+    
+    # membuat input tahun pilihan
+    col1, col2 = st.columns(2)
+    with col1:
+        start_year = st.number_input('Tahun Awal', min_value=1981, max_value=2024, value=1991, step=1)
+    with col2:
+        end_year = st.number_input('Tahun Akhir', min_value=1981, max_value=2024, value=2010, step=1)
 
-	# membuang NaN dan nilai negatif
-	valid_mask = ~np.isnan(pr_flat) & (pr_flat > 0)
-	lat_valid = lat_flat[valid_mask]
-	lon_valid = lon_flat[valid_mask]
-	pr_valid = pr_flat[valid_mask]
+    # narasi mengenai ketersediaan tahun pada dataset pilihan
+    if varname == 'Precipitation':
+        with st.expander("**Keterangan :**"):
+            st.caption("**Parameter :** *Curah Hujan (Precipitation).*")
+            st.caption("**Deskripsi :** *Dimulai dari tahun 1981 s.d 2024.*")
+    else:
+        with st.expander("**Keterangan :**"):
+            st.caption("**Parameter :** *Tmax (suhu maksimum) dan Tmin (suhu minimum).*")
+            st.caption("**Deskripsi :** *Dimulai dari tahun 1983 s.d 2016.*")
+    
+    if st.button('Download Data'):
+        st.session_state['download_files'] = []  # daftar file download untuk beberapa pilihan / tahun
+        download_and_process_data(dataname, varname, resolution, longitude, latitude, start_year, end_year)
 
-	# menyesuaikan warna
-	pr_categories = np.digitize(pr_valid, bins=precip_ranges) - 1
-	colors = px.colors.sequential.RdBu[::2][:len(precip_ranges)]
+    # Display tombol download
+    if 'download_files' in st.session_state and st.session_state['download_files']:
+        # narasi singkat
+        with st.expander('**Simpan file :**'):
+            st.caption('*File sudah siap disimpan ke direktori lokal dengan klik tombol di bawah*')
+        for idx, (file_path, file_name) in enumerate(st.session_state['download_files']):
+            with open(file_path, "rb") as file:
+                st.download_button(
+                    label=f"{file_name}",
+                    data=file,
+                    file_name=file_name,
+                    key=f"download_button_{idx}"  # Unique key untuk masing2 file
+                )
 
-	# membuat peta
-	fig_map = go.Figure()
-
-	# menambahkan marker, colorbar
-	fig_map.add_trace(go.Scattermapbox(
-		lat=lat_valid,
-		lon=lon_valid,
-		mode='markers',
-		marker=go.scattermapbox.Marker(
-			size=6,
-			color=pr_categories,
-			colorscale=colors,
-			cmin=0,
-			cmax=len(precip_ranges) - 1,
-			colorbar=dict(
-				title='Curah Hujan (mm)',
-				orientation='h',
-				x=0.5,
-				y=-0.15,
-				len=0.9,
-				thickness=15,
-				tickvals=np.arange(1, len(precip_ranges) - 1),
-				ticktext=precip_ranges[1:]
-			)
-		),
-		text=[f'Lintang: {lat}<br>Bujur: {lon}<br>Curah Hujan: {pr:.3f}' 
-			for pr, lat, lon in zip(pr_valid, lat_valid, lon_valid)],
-		hoverinfo='text'
-	))
-
-	# titik tengah koordinat
-	center_lat = -2 
-	center_lon = 118 
-
-	# Update layout with posisi dan ukuran
-	fig_map.update_layout(
-		mapbox=dict(
-			style="open-street-map",
-			center={"lat": center_lat, "lon": center_lon},
-			zoom=3.5,
-		),
-		width=1000,
-		height=600,
-		title={'text': f'Peta {name_var} Curah Hujan Bulanan Periode {selected_month}',
-				'x': 0.5, 'y': 0.9, 'xanchor': 'center', 'yanchor': 'top',
-					'font':{'size':20,'family':'Arial, sans-serif'}},
-		autosize=True,
-		margin={"r": 0, "t": 100, "l": 0, "b": 0}
-	)
-
-	# Display peta
-	st.plotly_chart(fig_map)
-	st.divider()
-
-	# menghitung jumlah pr per kategori
-	precip_counts = np.histogram(pr_valid, bins=precip_ranges)[0]
-
-	# membuat pie chart
-	fig_pie = go.Figure(data=[go.Pie(
-			labels=precip_labels,
-			values=precip_counts,
-			hole=0.4,
-			marker=dict(colors=colors)
-		)])
-
-	# update layout judul, legend
-	fig_pie.update_layout(
-		title=dict(
-			text=f'Distribusi Curah Hujan<br>periode {selected_month}',
-			font=dict(size=20),
-			x=0.5,
-			xanchor='center'
-		),
-		legend=dict(
-			title=dict(
-				text='milimeter (mm) :',
-				font=dict(size=14, family='Calibri, sans-serif')
-			),
-			x=0.5,
-			y=-0.1,
-			orientation='h',
-			yanchor='top',
-			xanchor='center',
-			traceorder='normal' # pilihan traceorder: 'normal', 'reversed', 'grouped', 'reversed+grouped'
-		)
-	)
-
-	# menambahkan keterangan pada hole pie chart
-	fig_pie.add_annotation(dict(
-		text=f'Total Titik<br>{sum(precip_counts)}',
-		x=0.5,
-		y=0.5,
-		font_size=15,
-		showarrow=False
-	))
-
-	# display chart
-	st.plotly_chart(fig_pie)
-	st.divider()
-
-	# membuat inputan koordinat
-	col = st.columns(2)
-	with col[0]:
-		selected_lat = st.number_input('Input Lintang', value=-2.000, format="%.3f", step=0.025, key='lat_input')
-	with col[1]:
-		selected_lon = st.number_input('Input Bujur', value=120.000, format="%.3f", step=0.025, key='lon_input')
-
-	# membuat narasi dalam keterangan
-	with st.expander(":blue-background[**Keterangan :**]"):
-		st.caption("*Ketik titik koordinat berdasarkan referensi koordinat dari peta reanalysis di atas (arahkan kursor di atas peta)*")
-		st.caption("*Atau bisa menambahkan atau mengurangi nilai dengan klik tanda tambah atau kurang*")
-		st.caption("*Apabila tidak muncul nilai pada line chart, berarti tidak ada nilai curah hujan (NaN) pada titik tersebut*")
-
-	# mencari nilai lat dan lon terdekat
-	lon_idx = np.abs(lon - selected_lon).argmin()
-	lat_idx = np.abs(lat - selected_lat).argmin()
-
-	pr_timeseries = pr[:, lat_idx, lon_idx].values
-
-	# konversi dataframe
-	df = pd.DataFrame({
-			'Rentang Waktu': pr.time.values,
-			'Curah Hujan (mm/hari)': pr_timeseries
-		})
-
-	# membuat line chart
-	fig_line = px.line(df, x='Rentang Waktu', y='Curah Hujan (mm/hari)')
-		
-	# Update layout judul dan posisi
-	fig_line.update_layout(
-			xaxis_title='Rentang Waktu',
-			yaxis_title='Curah Hujan (mm/hari)',
-			title={'text': f'Grafik Curah Hujan Harian Pada Koordinat {selected_lon} dan {selected_lat}',
-				'x': 0.5, 'y': 0.9, 'xanchor': 'center', 'yanchor': 'top',
-					'font':{'size':18,'family':'Arial, sans-serif'}},
-			margin={"r": 0, "t": 100, "l": 0, "b": 0}
-		)
-
-	# Display line chart
-	st.plotly_chart(fig_line)
-
-else:
-	st.write("Silahkan unggah file netCDF anda untuk visualisasi data.")
+if __name__ == '__main__':
+    main()
